1a663de0f3f39eb928192e743bf9f6ffb2e833ce
Isidoro Arostegui <iarostegui@sms-sudamerica.com>
2022-06-24 06:58:58 -0300

    init file commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './Utils.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>asdasdasdassa</h1>
+    </div>
+  )
+}
+
+export default App
